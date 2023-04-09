fe990334f83023310fe480ba4dcbbe568fb7f21b
kararag <107810255+kararag@users.noreply.github.com>
2023-04-09 19:46:38 +0530

    update: s3 bucket name for backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "demo-backend"
-    key     = "setu/terraform.tfstate"
+    key     = "sample-app-demo/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
